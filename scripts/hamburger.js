6e11eb221904f5e2778f163f77a4e06916284a28
Nathan-McComber <nathanzmcc@gmail.com>
2025-10-08 17:30:15 -0400

    Fixed mobile header - hopefully

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -3,16 +3,17 @@ var menuEnabled = false;
 // Turns the hamburger menu on/off
 function toggleMenu() {
     let menu = document.getElementById("hamburger-menu");
+    let nav = document.getElementsByTagName("nav")[0];
 
     // Show/hide menu
     if (!menuEnabled) {
         // SHOW MENU
-        menu.style.top = "5vh";
-
+        menu.style.top = nav.style.height + "5vh";
         menuEnabled = true;
+
     } else {
         // HIDE MENU
-        menu.style.top = "-12vh";
+        menu.style.top = "-16vh";
 
         menuEnabled = false;
     }
